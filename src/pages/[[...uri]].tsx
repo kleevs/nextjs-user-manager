d1a9d56698fd7ccc4a6d2ff199d7fa12f664f39b
Kevin Landa <kev.sam.landa@gmail.com>
2020-11-08 14:32:14 +0100

    _app

--- src/pages/[[...uri]].tsx
+++ src/pages/[[...uri]].tsx
@@ -1,35 +1,47 @@
 import React from 'react';
-import { GetServerSideProps } from 'next';
 import Head from 'next/head';
-import RouterModule, { getData } from 'src/module/router';
-
-export default function SigninPage({ uri, data }) {
-  return <Meta>
-    <RouterModule uri={uri} data={data} />
-  </Meta>
-}
-export const getServerSideProps: GetServerSideProps = async ({params : { uri }, req, res }) => {
-  const data = await getData({ uri }) || '';
-  if (req?.headers?.accept?.split(',').filter(_ => _ === 'text/html')[0]) {
-    return {
-      props: {
-        uri: `/${uri || ''}`,
-        data: data
-      }
-    };
-  } else {
-    res.writeHead(200, { 'Content-Type': 'text/json' });
-    res.write(JSON.stringify(data));
-    res.end();
-
-    return {
-      props: {}
-    };
-  } 
-}
+import dynamic from 'next/dynamic';
+import { Route } from "react-router-dom";
+import { Router } from 'src/tools/router';
+import { UserEngine } from 'src/engine/user.engine';
+import AjaxService from 'src/domain/api/ajax.service';
+const ListingModule = dynamic(() => import('src/module/list'));
+const DetailModule = dynamic(() => import('src/module/detail'));
 
 function Meta({children}) {
   return <>
     {children}
   </>
 }
+
+export default function Page({ uri, data, ...props }) {
+  return <Meta>
+    <Router uri={uri}>
+        <Route path='/users'>
+            <DetailModule user={data}/>
+        </Route>
+        <Route exact path='/'>
+            <ListingModule users={data}/>
+        </Route>
+    </Router>
+  </Meta>
+}
+
+const getInitialProps = async ({ asPath }) => {
+  const userEngine = new UserEngine(new AjaxService());
+  const uri = asPath?.split('/').filter(_ => _);
+  console.log(uri);
+  let data = null;
+  if (!uri || uri.length <= 0) {
+    data = await userEngine.list();
+  } else if (uri[0] === 'users') {
+    data = await userEngine.get(+uri[1] || 0) || {};
+  }
+
+  return {
+    uri: `/${uri?.join('/') || ''}`,
+    data: data
+  };
+}
+
+Page.getInitialProps = getInitialProps;
