70126c1a0e842ea2ea8a9e0f80f68ab1002f52bf
Kevin Landa <kev.sam.landa@gmail.com>
2021-03-28 13:42:35 +0200

    mobile version

--- app-server/src/index.tsx
+++ app-server/src/index.tsx
@@ -7,26 +7,21 @@ import { MobileModule, Page } from 'user-manager-react/src/module/mobile'
 import { useComponent } from "user-manager-react/src/hook/use-component";
 
 function App({}: {}) {
-    const navigate = (href: string) => { location.href = href; }
-    return <Router>
-        <Switch>
-            <Route exact path='/users' render={() => <DetailModule navigate={navigate} id={0} />} />
-            <Route exact path='/users/:id' render={({match: { params: { id }}}) => <DetailModule navigate={navigate} id={+id} />} />
-            <Route path="/" render={() => <ListModule navigate={navigate} />} />
-        </Switch>
-    </Router>
-}
-
-function AppMobile({}: {}) {
     const Content = useComponent(() => {
         const history = useHistory();
         const navigate = (href: string) => { history.push(href); }
+        const navigateMobile = (href: string) => { 
+            history.push(`/mobile${href}`); 
+        }
         return <Switch>
-            <Route exact path='/users' render={({match: { params: { }}}) => <MobileModule id={0} navigate={navigate} page={Page.Detail} />} />
-            <Route exact path='/users/:id' 
-                render={({match: { params: { id }}}) => <MobileModule id={+id} navigate={navigate} page={Page.Detail} />} />
-            <Route exact path={['/']} 
-                render={() => <MobileModule id={0} navigate={navigate} page={Page.List} />} />
+            <Route exact path='/users' render={() => <DetailModule navigate={navigate} id={0} />} />
+            <Route exact path='/users/:id' render={({match: { params: { id }}}) => <DetailModule navigate={navigate} id={+id} />} />
+
+            <Route exact path='/mobile/users' render={() => <MobileModule navigate={navigateMobile} id={0} page={Page.Detail} />} />
+            <Route exact path='/mobile/users/:id' render={({match: { params: { id }}}) => <MobileModule navigate={navigateMobile} id={+id} page={Page.Detail} />} />
+            <Route path="/mobile" render={() => <MobileModule id={0} navigate={navigateMobile} page={Page.List} />} />
+            
+            <Route path="/" render={() => <ListModule navigate={navigate} />} />
         </Switch>
     })
 
@@ -35,6 +30,6 @@ function AppMobile({}: {}) {
     </Router>
 }
 
-render(<AppMobile/>, document.getElementById("app"));
+render(<App/>, document.getElementById("app"));
 
 
