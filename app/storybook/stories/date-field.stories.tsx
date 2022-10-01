38d29759157945a380d705129815ef50b09878d5
Kevin Landa <kev.sam.landa@gmail.com>
2022-10-01 18:33:43 +0200

    add storybook

---
+++ app/storybook/stories/date-field.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import  { DateField } from "lib-ui";
+
+export default {
+  title: 'Example/DateField',
+  component: DateField,
+  argTypes: {
+    label: 'label',
+    error: 'error',
+    value: new Date(),
+    onChange: (v: Date) => {}
+  },
+};
+
+const Template = (args) => <DateField {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  label: 'label',
+  error: 'error',
+  value: new Date(),
+  onChange: (v: Date) => {}
+};
